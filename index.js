da3096578224bd64543752ba5a5a33687a1c4f9b
DEBAJYOTI <ds2250@it.jgec.ac.in>
2022-02-13 00:57:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById("donate").onclick = function () {
+    location.href = "https://rzp.io/l/kIjVnUnjsy";
+};
+
+document.getElementById("donate1").onclick = function () {
+    location.href = "https://rzp.io/l/kIjVnUnjsy";
+};
+document.getElementById("submit").onclick = function () {
+    window.location.href="index.html";
+};
+
+
